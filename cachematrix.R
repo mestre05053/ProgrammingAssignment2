dde083418bc30d3d8cbdef621be54b9515730169
mestre05053 <63665431+mestre05053@users.noreply.github.com>
2020-11-19 16:24:14 +0000

    Assigment Done

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,38 @@
-## Put comments here that give an overall description of what your
-## functions do
+makeCacheMatrix <- function(m_in = matrix()) {
+    #Creating a NULL Matrix
+    m <- NULL
 
-## Write a short comment describing this function
+    #Exporting the Matrix  
+  matrix_set <- function(m_out) {
+          m_in <<- m_out
+          m <<- NULL
+  }
+  
+  #Defining the return matrix 
+  matrix_get <- function() m_in
 
-makeCacheMatrix <- function(x = matrix()) {
-
+  #Getting the Inverse Matrix
+  matrix_set_Inverse <- function(inverse) m <<- inverse
+  matrix_get_inverse <- function() m
+  
+  #Creating the list of objects 
+  list(matrix_set = matrix_set,
+       matrix_get = matrix_get,
+       matrix_set_Inverse = matrix_set_Inverse,
+       matrix_get_inverse = matrix_get_inverse)
 }
 
-
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(m_in, ...) {
+  
+  # Setting Inverted Matrix 
+  m <- m_in$matrix_get_inverse()
+  #Checking if the matrix was already computed
+  if (!is.null(m)) {
+          message("Getting cached Matrix")
+          return(m)
+  }
+  data <- m_in$matrix_get()
+  m <- solve(data, ...)
+  m_in$matrix_set_Inverse(m)
+  m
 }
